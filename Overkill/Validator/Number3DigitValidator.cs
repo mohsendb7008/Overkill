9a99abc71385d7960afa8f43b7bb8a63edc367ee
mohsendb7008 <mohsendb7008@gmail.com>
2025-07-10 23:23:03 +0200

    Fix validator

--- Overkill/Validator/Number3DigitValidator.cs
+++ Overkill/Validator/Number3DigitValidator.cs
@@ -4,15 +4,10 @@ namespace Overkill.Validator;
 
 public class Number3DigitValidator : IValidator<int>
 {
-    public void Validate(int item)
+    public bool Validate(int item) => item is < 100 or > 999;
+    public void ValidateOrThrow(int item)
     {
-        if (item is < 100 or > 999)
-            throw new InvalidArgumentException("Provider number is not a valid 3 digit number.");
-    }
-
-    public Task ValidateAsync(int item)
-    {
-        Validate(item);
-        return Task.CompletedTask;
+        if (!Validate(item))
+            throw new InvalidArgumentException("Provided number is not a valid 3 digit number.");
     }
 }
