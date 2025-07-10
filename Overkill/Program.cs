7446c706ff2ae25bbde2b54afbf416fd32372dd9
mohsendb7008 <mohsendb7008@gmail.com>
2025-07-10 23:56:48 +0200

    email and phone validator usage

--- Overkill/Program.cs
+++ Overkill/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("TODO!");
+﻿using Overkill.Validator;
+
+var emailValidator = new EmailValidator();
+Console.WriteLine(emailValidator.Validate("sample@school.edu"));
+Console.WriteLine(emailValidator.Validate("invalid@invalid"));
+
+var phoneValidator = new PhoneValidator();
+Console.WriteLine(phoneValidator.Validate("09215546321"));
+Console.WriteLine(phoneValidator.Validate("093311111111"));
