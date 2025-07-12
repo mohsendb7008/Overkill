547026ca75803bbd6d41a8a363bf8ff364eea193
mohsendb7008 <mohsendb7008@gmail.com>
2025-07-13 01:18:24 +0200

    hidden equation usage

--- Overkill/Program.cs
+++ Overkill/Program.cs
@@ -1,9 +1,20 @@
-﻿using Overkill.Validator;
+﻿using Overkill.Reader;
+using Overkill.Transformer;
+using Overkill.Validator;
 
-var emailValidator = new EmailValidator();
-Console.WriteLine(emailValidator.Validate("sample@school.edu"));
-Console.WriteLine(emailValidator.Validate("invalid@invalid"));
-
-var phoneValidator = new PhoneValidator();
-Console.WriteLine(phoneValidator.Validate("09215546321"));
-Console.WriteLine(phoneValidator.Validate("093311111111"));
+var reader = new ConsoleReader();
+var equation = reader.Read();
+var equationTransformer = new SimpleEquationTransformer();
+var variables = equationTransformer.Transform(equation);
+var hiddenTransformer = new HiddenEquationNumberTransformer(new NumberDigitRangeHiddenValidator());
+var hiddenNumber = hiddenTransformer.Transform(variables);
+var hiddenRangeMatch = new NumberDigitRangeMatchValidator();
+if (!hiddenRangeMatch.Validate((variables[hiddenNumber.Item1], hiddenNumber.Item2)))
+{
+    Console.WriteLine("-1");
+}
+else
+{
+    variables[hiddenNumber.Item1] = hiddenNumber.Item2.ToString();
+    Console.WriteLine($"{variables[0]} + {variables[1]} = {variables[2]}");
+}
