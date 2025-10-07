70ff49422b023685ff7f464f542eda1cac36bf87
mohsendb7008 <mohsendb7008@gmail.com>
2025-10-07 18:35:28 +0200

    update and delete api for library client

--- Overkill/Domains/Library/LibraryClient.cs
+++ Overkill/Domains/Library/LibraryClient.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 
 namespace Overkill.Domains.Library;
@@ -34,5 +35,44 @@ public class LibraryClient
             return cantRecognize;
         var category = books[0].Category;
         return books.All(book => book.Category == category) ? category : cantRecognize;
+    }
+
+    public async Task<string> UpdateBookAsync(int id, Book book, string url)
+    {
+        if (id < 1)
+            return "Given id is not valid";
+        if (string.IsNullOrWhiteSpace(book.Title))
+            return "Book title can not be empty";
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Put,
+            RequestUri = new Uri(url),
+            Content = JsonContent.Create(book)
+        };
+        var response = await _client.SendAsync(request);
+        return response.StatusCode switch
+        {
+            HttpStatusCode.OK => "Book updated successfully",
+            HttpStatusCode.NotFound => "Book not found",
+            _ => "Server Error"
+        };
+    }
+
+    public async Task<string> DeleteBookAsync(int id, string url)
+    {
+        if (id < 1)
+            return "Id is not valid";
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Delete,
+            RequestUri = new Uri(url)
+        };
+        var response = await _client.SendAsync(request);
+        return response.StatusCode switch
+        {
+            HttpStatusCode.OK => "Book deleted successfully",
+            HttpStatusCode.NotFound => "Book to delete was not found",
+            _ => "Server Error"
+        };
     }
 }
